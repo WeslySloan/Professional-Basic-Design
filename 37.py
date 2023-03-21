997e8e4a98fe059a11307bd4e776757994de33b3
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2023-03-21 23:40:13 +0900

    add 37,38

---
+++ 37.py
@@ -0,0 +1,7 @@
+a, b  = input().split()
+a = int(a)
+b = int(b)
+Quo = a//b
+Rem = a%b
+
+print("Quotient: " + format(Quo, ".0f") + ", Remainder: " + format(Rem, ".0f"))
---
+++ 38.py
@@ -0,0 +1,8 @@
+r1, r2  = input().split()
+r1 = int(r1)
+r2 = int(r2)
+
+r = ( r1 * r2 ) / ( r1 + r2 )
+
+
+print(format(r, ".2f"))
